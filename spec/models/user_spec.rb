d4b3a57e4f0cc48cf2d109a3a7cc2bcf1ebe78e5
sehrguey ogoltsoff <ogoltsoff@gmail>
2012-04-29 13:51:28 +0400

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,13 +8,14 @@
 #  created_at :datetime        not null
 #  updated_at :datetime        not null
 #
-
 require 'spec_helper'
 
 describe User do
- 
-  before { @user = User.new(name: "Example User", email: "user@example.com",
-                            password: "foobar", password_confirmation: "foobar") }
+
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com", 
+                     password: "foobar", password_confirmation: "foobar")
+  end
                             
        subject { @user }
        
@@ -23,7 +24,8 @@ describe User do
        it { should  respond_to(:password_digest) }
        it { should respond_to(:password) }
        it { should  respond_to(:password_confirmation) }
-       it { should respond_to(:authenticate) }
+       it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }  
        
        it { should be_valid }
        
@@ -116,4 +118,9 @@ describe User do
       specify { user_for_invalid_password.should be_false }
     end
   end
+ describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 end
+
